977a9b43fa1b952d285a379f37a8c30aff6c7b0c
Matthew Francis Brunetti <zenflow87@gmail.com>
2015-05-31 22:49:22 -0400

    Real initial commit

---
+++ tests/dummy.js
@@ -0,0 +1,14 @@
+if (process.browser){
+	console.log('I am on the server');
+} else {
+	console.log('I am on the client');
+}
+
+console.log('Again, I am on the ' + (process.browser ? 'client' : 'server'));
+
+console.log('My secret token is ' + (!process.browser && '"secret token"' || '..jk, not telling'));
+
+function animate(){
+	if (!process.browser){return;}
+	doThis().then(doThat);
+}
