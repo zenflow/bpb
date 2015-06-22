70588fd7d763ed52d81d5cff373c6d9b7219f0d6
Matthew Francis Brunetti <zenflow87@gmail.com>
2015-06-22 13:25:26 -0400

    simplify/refactor tests

--- tests/index.js
+++ tests/index.js
@@ -1,57 +1,23 @@
 var test = require('tape');
-var bpb = require('../lib');
 var concat = require('concat-stream');
 var fs = require('fs');
 var path = require('path');
+var bpb = require('../lib');
 
-test('dummy a (easy)', dummyTest('a'));
-test('dummy b (trickier)', dummyTest('b'));
+test('test-dummy module a (easy)', dummyTest('a'));
+test('test-dummy module b (trickier)', dummyTest('b'));
 
-function dummyTest(name) {
+function dummyTest(module_name) {
     return function (t) {
         t.plan(1);
-        asyncBoth(function(cb){
-            apply(name, cb);
-        }, function(cb){
-            fs.readFile(path.join(__dirname, 'output', name+'.js'), 'utf8', cb);
-        }, function(error, actual, expected){
-            if (error){return t.fail(error);}
-            t.equal(stripWhitespace(expected), stripWhitespace(actual));
-        });
-    };
-}
-
-function asyncBoth(taskA, taskB, cb) {
-    var task_a_results, task_b_results;
-    var _cb = function (error) {
-        if (error) {
-            return cb(error);
-        }
-        if (task_a_results && task_b_results) {
-            cb.apply(null, [null].concat(task_a_results, task_b_results));
-        }
-    };
-    taskA(function (error) {
-        if (error) {
-            return _cb(error)
-        }
-        task_a_results = Array.prototype.slice.call(arguments, 1);
-        _cb();
-    });
-    taskB(function (error) {
-        if (error) {
-            return _cb(error)
-        }
-        task_b_results = Array.prototype.slice.call(arguments, 1);
-        _cb();
-    });
-}
-
-function apply(file, cb){
-    var in_stream = fs.createReadStream(path.join(__dirname, 'input', file+'.js'), {encoding: 'utf8'});
-    in_stream.pipe(bpb()).pipe(concat(function(data){
-        cb(null, data.toString('utf8'));
-    }));
+        var expected;
+        fs.createReadStream(path.join(__dirname, 'input', module_name+'.js'), {encoding: 'utf8'})
+            .pipe(bpb())
+            .pipe(concat(function(data){
+                t.equal(stripWhitespace(data.toString('utf8')), stripWhitespace(expected));
+            }));
+        expected = fs.readFileSync(path.join(__dirname, 'expected', module_name+'.js'), 'utf8');
+    }
 }
 
 function stripWhitespace (text){
