d2081bd70e6fd389ad541ee22cb432e5012c664a
Matthew Francis Brunetti <zenflow87@gmail.com>
2015-06-27 09:15:55 -0400

    exclude occurrences where `process` is overridden by function declaration

--- tests/index.js
+++ tests/index.js
@@ -7,24 +7,32 @@ test('works', function(t){
     testSource(t, '(function(){return !process.browser ? 1 : 2; })(); ',
                 '(function(){return !true ? 1 : 2; })(); ');
 });
-test('disqualifies references in scope where `process` identifier is overridden', function(t){
-    t.plan(9);
+test('excludes occurrences in scope where `process` identifier is overridden', function(t){
+    t.plan(12);
     testSource(t, 'foo(process.browser); var process; ');
+    testSource(t, 'foo(process.browser); function process(){} ');
     testSource(t, '(function(){foo(process.browser); })(); var process; ');
-    testSource(t, '(function(process){foo(process.browser); })(); ');
+    testSource(t, '(function(){foo(process.browser); })(); function process(){} ');
     testSource(t, '(function(){foo(process.browser); var process; })(); ');
+    testSource(t, '(function(){foo(process.browser); function process(){} })(); ');
+    testSource(t, '(function(process){foo(process.browser); })(); ');
+    testSource(t, '(function process(){foo(process.browser); })(); ');
+    // nested function scopes
     testSource(t, '(function(){(function(){foo(process.browser); })(); })(); var process; ');
+    testSource(t, '(function(){(function(){foo(process.browser); })(); })(); function process(){} ');
     testSource(t, '(function(process){(function(){foo(process.browser); })(); })(); ');
-    testSource(t, '(function(){(function(){foo(process.browser); })(); var process; })(); ');
-    testSource(t, '(function(){(function(process){foo(process.browser); })(); })(); ');
-    testSource(t, '(function(){(function(){foo(process.browser); var process; })(); })(); ');
+    testSource(t, '(function process(){(function(){foo(process.browser); })(); })(); ');
 });
-test('includes references where `process` is overridden in subscope', function(t){
-    t.plan(2);
+test('includes occurrences where `process` is overridden in subscope', function(t){
+    t.plan(4);
     testSource(t, '(function(){var process; })(); foo(process.browser); ',
-                '(function(){var process; })(); foo(true); ');
+                  '(function(){var process; })(); foo(true); ');
+    testSource(t, '(function(){function process(){} })(); foo(process.browser); ',
+                  '(function(){function process(){} })(); foo(true); ');
     testSource(t, '(function(process){})(); foo(process.browser); ',
-                '(function(process){})(); foo(true); ');
+                  '(function(process){})(); foo(true); ');
+    testSource(t, '(function process(){})(); foo(process.browser); ',
+                  '(function process(){})(); foo(true); ');
 });
 
 function testSource(t, source, expected){
